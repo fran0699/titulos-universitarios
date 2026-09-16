1f82639ba0c7281c10352f78df0b8339a505cf49
Francisco Lata <francisco_0699@hotmail.com>
2026-09-16 10:19:51 -0500

    Sprint 1: proyecto base Spring Boot

---
+++ src/main/java/pa/edu/utp/titulos_universitarios/TitulosUniversitariosApplication.java
@@ -0,0 +1,13 @@
+package pa.edu.utp.titulos_universitarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitulosUniversitariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitulosUniversitariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pa/edu/utp/titulos_universitarios/TitulosUniversitariosApplicationTests.java
@@ -0,0 +1,13 @@
+package pa.edu.utp.titulos_universitarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitulosUniversitariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
